745a5ed30b6df7fade346bef13bceb15533bb5b6
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-14 23:39:29 +0600

    cleand up the code

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -49,7 +49,6 @@ const Header = () => {
       
     </ul>
         </div>
-        {/* <div className="dropdown justify-end"> */}
       <label htmlFor='dashboard-drawer' tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
