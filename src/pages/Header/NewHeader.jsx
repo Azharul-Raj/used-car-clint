3eee338bcb72d34bc6f2f72072d72b0e7024ebf0
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-06 15:34:53 +0600

    start custom header designing

---
+++ src/pages/Header/NewHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NewHeader() {
+  return (
+    <div>
+        {/* logo part */}
+        <div className="">
+            <h5>UsedCarZone</h5>
+        </div>
+        {/* nav part */}
+    </div>
+  )
+}
